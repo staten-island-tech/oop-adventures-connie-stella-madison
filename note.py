1bf08ad82782e2c0fd676fe80001ff9143ebc688
ConnieLiao <conniel32@nycstudents.net>
2025-01-10 10:15:57 -0500

    Update note.py

---
+++ note.py
@@ -0,0 +1,4 @@
+-f string helps u fill in something that in {} without overwriting it
+- _init_ helps set things up when creating something new in python
+-enumerate helps python give me the number along with what the number brings to make it easier and simpler
+ 
